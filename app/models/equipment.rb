b6514baa4e1edae732bab64233a0d97c984d93eb
Vladislavs Rudāns <vladislavs.rudans@visma.com>
2022-10-07 16:13:10 +0300

    Not needed table_name if we use inflections.

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,6 +1,4 @@
 class Equipment < ApplicationRecord
-  self.table_name = "equipment"
-
   validates :name, :equipment_type, presence: true
   has_many :operation, dependent: :delete_all
   accepts_nested_attributes_for :operation
